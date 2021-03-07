187001a48d16bb14c46bb0ec0b3b90f4e9c1b4a6
Gabriel Alvares Gibran <gibran800@outlook.com>
2021-03-07 03:15:10 -0300

    Added the game files

---
+++ Assets/State.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")] // Creates a "State" option in "Create", in Assets.
+public class State : ScriptableObject
+{
+    [TextArea(20, 30)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates; // Storying the next states. The contents of this array will have access to storyText.
+
+    // Getters:
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
